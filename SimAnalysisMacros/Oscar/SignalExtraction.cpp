99d3ed3110a4476eb7fce5da467c6c1c20c0a26e
omatamala <oromero@gatech.edu>
2020-06-08 11:52:51 -0400

    modified:   AGET_out_pulse.txt 	modified:   MUSIC_out_pulse.txt 	modified:   ../SimAnalysisMacros/Oscar/SignalExtraction.cpp

--- SimAnalysisMacros/Oscar/SignalExtraction.cpp
+++ SimAnalysisMacros/Oscar/SignalExtraction.cpp
@@ -35,6 +35,7 @@ int SignalExtraction (){
 
 	bool *isTriggered=0;
 	int cEvent;
+	int nEvents=10000;
 
 	double sum_base=0;
 	double avg_base;
@@ -50,7 +51,7 @@ int SignalExtraction (){
 	TH2 *h_camera_integ = new TH2F ("h_camera_integ", "Camera Charge", 32,-0.5,32.5,16,-0.5,15.5);
 	TH1 *h_base = new TH1F("h_base", "Pedestal", 4096,0,4096);
 	TH1 *h_rms = new TH1F("h_rms", "RMS Of Pedestal", nPixels,0,nPixels);
-	TH1 *h_rms_dist = new TH1F("h_rms_dist","Camera RMS", 4096,0,4096);
+	TH1 *h_rms_dist = new TH1F("h_rms_dist","Camera RMS", 1000,0,10);
 	TTree *tSimulatedEvents = (TTree*)file->Get( "Events/tSimulatedEvents" );
 	TTree *tEvent = (TTree*)file->Get( "Events/T0" );
 	TGraph *gr_mean = new TGraph();
@@ -76,29 +77,40 @@ int SignalExtraction (){
 
 	tSimulatedEvents->GetEntry(9574);
 	tEvent->GetEntry(9574);
-	for (int i=0; i<nPixels; i++){
-		sum_base=0;
-		px_rms=0;
-		FindBin(i,&nx,&ny);
-		for (int j = 0; j<nSampleOffset; j++){
-			sum_base += iTrace[i]->at(j);
-			px_rms += iTrace[i]->at(j)*iTrace[i]->at(j);
-			gr_mean->SetPoint(nSampleOffset*i+j,i,iTrace[i]->at(j));
-			h_base->Fill(iTrace[i]->at(j));
+	for (int k = 0; k<nEvents; k++){
+		tSimulatedEvents->GetEntry(k);
+		tEvent->GetEntry(k);
+		if(isTriggered){
+			h_base->Reset();
+			h_camera_integ->Reset();
+			for (int i=0; i<nPixels; i++){
+				sum_base=0;
+				//px_rms=0;
+				FindBin(i,&nx,&ny);
+				for (int j = 0; j<nSampleOffset; j++){
+					sum_base += iTrace[i]->at(j);
+					//px_rms += iTrace[i]->at(j)*iTrace[i]->at(j);
+					gr_mean->SetPoint(nSampleOffset*i+j,i,iTrace[i]->at(j));
+					h_base->Fill(iTrace[i]->at(j));
+				}
+				avg_base = sum_base/nSampleOffset;
+				//px_rms = sqrt(px_rms/nSampleOffset);
+				
+				//cout << avg_base << endl;
+				h_camera->SetBinContent(nx,ny,iPePix->at(i));
+		
+				integ = 0;
+				for (int j = 0; j<iTrace[i]->size(); j++){
+					integ += iTrace[i]->at(j)-avg_base;
+				}
+				h_camera_integ->SetBinContent(nx,ny,integ);
+			}
+			//h_rms->Fill(,h_base->GetRMS());
+			//h_rms->SetBinError(h_rms->FindBin(k),0);
+			h_rms_dist->Fill(h_base->GetRMS());
+			//cout<<h_base->GetRMS()<<endl;
+			//cout<<k<<endl;
 		}
-		avg_base = sum_base/nSampleOffset;
-		px_rms = sqrt(px_rms/nSampleOffset);
-		h_rms->Fill(i,px_rms);
-		h_rms->SetBinError(h_rms->FindBin(i),0);
-		h_rms_dist->Fill(px_rms);
-		//cout << avg_base << endl;
-		h_camera->SetBinContent(nx,ny,iPePix->at(i));
-
-		integ = 0;
-		for (int j = 0; j<iTrace[i]->size(); j++){
-			integ += iTrace[i]->at(j)-avg_base;
-		}
-		h_camera_integ->SetBinContent(nx,ny,integ);
 	}
 
 	TCanvas *c_camera = new TCanvas("c_camera", "Camera Canvas",800,500);
@@ -114,28 +126,29 @@ int SignalExtraction (){
 	gr_mean->GetHistogram()->GetYaxis()->SetTitle("Pedestal");
 	gr_mean->Draw("P");
 
-	TCanvas *c_rms = new TCanvas("c_rms", "Canvas RMS", 800,500);
+	/*TCanvas *c_rms = new TCanvas("c_rms", "Canvas RMS", 800,500);
 	h_rms->Draw("HIST");
 	h_rms->SetStats(0);
 	h_rms->GetXaxis()->SetTitle("Pixel");
 	h_rms->GetYaxis()->SetTitle("RMS");
 	h_rms->SetStats(0);
-	h_rms->SetMarkerStyle(kFullCircle);
+	h_rms->SetMarkerStyle(kFullCircle);*/
 
 	TCanvas *c_base = new TCanvas("c_base", "Canvas Baseline", 800,500);
 	h_base->Draw("HIST");
 	h_base->SetStats(0);
-	h_base->GetXaxis()->SetTitle("Pixel");
+	h_base->GetXaxis()->SetTitle("DC");
 	h_base->GetYaxis()->SetTitle("Frequency");
 	h_base->GetXaxis()->SetRangeUser(490,510);
 
 	
-	h_rms_dist->Draw("SAME HIST");
+	TCanvas *c_rms = new TCanvas("c_rms", "Canvas RMS", 800,500);
+	h_rms_dist->Draw("HIST");
 	h_rms_dist->SetStats(0);
 	h_rms_dist->SetLineColor(kBlack);
-	h_rms_dist->GetXaxis()->SetTitle("Pixel");
+	h_rms_dist->GetXaxis()->SetTitle("RMS");
 	h_rms_dist->GetYaxis()->SetTitle("Frequency");
-	h_rms_dist->GetXaxis()->SetRangeUser(490,510);	
+	h_rms_dist->GetXaxis()->SetRangeUser(0,10);	
 
 	return 0;
 }
