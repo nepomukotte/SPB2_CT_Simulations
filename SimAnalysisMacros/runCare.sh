a1305e249fea66e54092d5dc51e5d33beea345c9
Nepomuk <nepomuk.otte@gmail.com>
2020-06-08 09:08:59 -0400

    changes in runcare.sh

--- SimAnalysisMacros/runCare.sh
+++ SimAnalysisMacros/runCare.sh
@@ -1,3 +1,4 @@
-../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera.cfg -of ../data/test -if /home/nepi/Share/POEMMA/SPB2/SPB2_CT_Simulations/OpticlessPointSourceInFP/photonLocation.root
+#../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera.cfg -of ../data/test -if /home/nepi/Share/POEMMA/SPB2/SPB2_CT_Simulations/OpticlessPointSourceInFP/photonLocation.root
+../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera.cfg -of ../data/test -if /home/nepi/Share/POEMMA/SPB2/SPB2_CT_Simulations/data/parallel_axis_dist_100m.root
 #../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera.cfg -of ../data/test_cher_cam_out -if ../data/test_cher_cam.root 
 #../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera_ElevationSplit.cfg -of ../data/TriggerEfficiencyElevationSplit -if /home/nepi/Share/POEMMA/SPB2/SPB2_CT_Simulations/OpticlessPointSourceInFP/SPB2PointSourceSimsElevationSplit.root 
