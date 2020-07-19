adff4a81643612ff60f1697f4b5aa888515ace9c
Mahdi Bagheri <mb15@wildcats.unh.edu>
2020-07-19 10:54:32 -0400

    added the resolution and efficiency calculations

--- SimAnalysisMacros/runCare.sh
+++ SimAnalysisMacros/runCare.sh
@@ -1,4 +1,5 @@
-../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera.cfg -of ../data/test -if /home/mahdi/Programs/SPB2/SPB2_CT_Simulations/OpticlessPointSourceInFP/SPB2PointSourceSims.root
+../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera_Oscar.cfg -of ../data/test -if /home/mahdi/Programs/SPB2/SPB2_CT_Simulations/OpticlessPointSourceInFP/SinglePeCherenkov.root
+#../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera.cfg -of ../data/test -if /home/mahdi/Programs/SPB2/SPB2_CT_Simulations/OpticlessPointSourceInFP/SPB2PointSourceSims.root
 #../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera.cfg -of ../data/test -if /home/nepi/Share/POEMMA/SPB2/SPB2_CT_Simulations/data/parallel_axis_dist_100m.root
 #../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera.cfg -of ../data/test_cher_cam_out -if ../data/test_cher_cam.root 
 #../../CARE/CameraAndReadout -s 3242 -c ../CAREConfig/SPB2_CARE_Camera_ElevationSplit.cfg -of ../data/TriggerEfficiencyElevationSplit -if /home/nepi/Share/POEMMA/SPB2/SPB2_CT_Simulations/OpticlessPointSourceInFP/SPB2PointSourceSimsElevationSplit.root 
